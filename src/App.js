15d44a5480ef241f5aca14a60739e84947e0ad9e
daniel <dmendezp@live.com.mx>
2022-10-12 08:17:41 -0500

    agregamos TodoCounter01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
 
-function App() {
+// import './App.css';
+
+const todos = [
+  {text: 'reparar pc', completed: false},
+  {text: 'reparar epson', completed: false},
+  {text: 'revisar hp all in one', completed: false},
+  {text: 'reparar laptop', completed: false},
+]
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      
+    <TodoCounter/>
+    {/* <TodoSearch/> */}
+    <input placeholder="Ejemplo"/>
+    {/* <TodoList>
+      {todos.map(todo =>(
+        // <TodoItem/>
+      ))} */}
+    {/* </TodoList> */}
+    {/* <CreateTodoButton/> */}
+    <button>+</button>
+    </React.Fragment>
   );
+    
 }
 
 export default App;
---
+++ src/TodoCounter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoCounter() {
+    return (
+        <h2>Has Completado 3 a de 5 ToDos</h2>
+    );
+}
+
+export  {TodoCounter};
