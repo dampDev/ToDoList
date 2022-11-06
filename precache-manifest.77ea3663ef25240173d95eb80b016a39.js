d8ca659c03e944955fe578da65e5b718b7269a8e
daniel <dmendezp@live.com.mx>
2022-11-06 13:05:24 -0600

    Updates

---
+++ precache-manifest.77ea3663ef25240173d95eb80b016a39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70e9919e9feb186b52c0",
+    "url": "/todolist/static/css/main.7900ed5d.chunk.css"
+  },
+  {
+    "revision": "70e9919e9feb186b52c0",
+    "url": "/todolist/static/js/main.70e9919e.chunk.js"
+  },
+  {
+    "revision": "12075b860204c87597b1",
+    "url": "/todolist/static/js/1.12075b86.chunk.js"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "97a90ae5d9d9f4ae389b5924da08b488",
+    "url": "/todolist/index.html"
+  }
+];
