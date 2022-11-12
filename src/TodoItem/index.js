52090613d2dbe0e9ca87a94501da3ce1f7d8efa6
daniel <dmendezp@live.com.mx>
2022-11-12 15:57:39 -0600

    intregramos react-icons

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,33 +1,35 @@
 import React from "react";
 import './TodoItem.css';
+import { FaCheckCircle, FaRegCircle, FaTrashAlt } from "react-icons/fa"
 
 function TodoItem(props) {
 
-   
+
 
     return (
         <li className="TodoItem">
             <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-            onClick={props.onComplete}
+                onClick={props.onComplete}
             >
-                c
+                {!props.completed && <FaRegCircle />}
+                {props.completed && <FaCheckCircle />}
             </span>
             <div className="TodoItem--content">
-            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>
-                {props.text}
-            </p>
-            <p className="TodoItem-date">{props.date}</p>
+                <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>
+                    {props.text}
+                </p>
+                <p className="TodoItem-date">{props.date}</p>
             </div>
-            
+
             <span className="Icon Icon-delete"
-            onClick={props.onDelete}
+                onClick={props.onDelete}
             >
-                X
+                <FaTrashAlt/>
             </span>
-            
+
         </li>
     );
-    
+
 }
 
-export  {TodoItem};
+export { TodoItem };
